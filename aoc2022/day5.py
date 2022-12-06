7fd3e5a687577f7ee651d83e1680068e2a7787c1
Ivan Gusiev <igusiev@spotify.com>
2022-12-06 17:01:21 +0100

    Refactored gif generation

--- aoc2022/day5.py
+++ aoc2022/day5.py
@@ -1,27 +1,26 @@
 from aoc2022.advent import set_day_from_filename
+from aoc2022.gif import initialize_gif, request_frame, save_gif, GIF_FONT
 from aoc2022.util import Input, split_by_newline
 from io import TextIOWrapper
-from PIL import Image, ImageDraw, ImageFont
+from PIL import ImageDraw
 import os
-from typing import Optional, Tuple, Set
 
-images = []
-image_width = 0
-image_height = 0
-unique_counter = 0
-image_day = ""
+#sizes
 BOX_WIDTH = 10
 BOX_HEIGHT = 7
 ADJUST_X = 3
 ADJUST_Y = -2
 BORDER = 5
-IMG_FONT = ImageFont.truetype("./res/font.ttf")
+
+# colors
+BACKGROUND = (0, 0, 100)
+HIGHLIGHT = (255, 0, 100)
+MAIN_FILL = (0, 255, 100)
+GREEN = (0, 255, 100)
 
 def main():
-    global image_day
     set_day_from_filename(__file__)
     input = Input.for_advent()
-    image_day = input.day
     for file in [input.test_path, input.challenge_path]:
         print("input:", file)
         with open(file, mode='r') as f:
@@ -47,7 +46,7 @@ def solve_p1(lines: list[str]):
     for stack in stacks:
         result += stack[-1]
 
-    save_gif("p1")
+    save_gif("p1", loop=0, duration=25)
     print("p1", result)
 
 def solve_p2(lines: list[str]):
@@ -81,26 +80,22 @@ def load_stacks(data: list[str], stacks: list[list[str]]):
                 stack.append(letter)
 
 def initialize_images(stacks: list[list[int]]):
-    global image_height, image_width, images, unique_counter
     columns = len(stacks)
-    rows = max(map(len, stacks)) * 5
-    print(rows)
-    image_width = (columns + 1) * (BOX_WIDTH + BORDER)
+    rows = max(map(len, stacks)) * 5  # 5 times max size for safety (not enough tho)
+    image_width = BORDER + columns * (BOX_WIDTH + BORDER)
     image_height = (rows + 2) * (BOX_HEIGHT + BORDER)
-    images = []
-    unique_counter += 1
-
-def append_empty_image() -> Tuple[int, Image.Image]:
-    global image_height, image_width, images
-    img = Image.new( 'RGB', (image_width, image_height), "black") # Create a new black image
-    images.append(img)
-    return len(images), img
+    initialize_gif(image_width, image_height)
 
 def draw_stacks(stacks: list[list[int]], highlights = None):
-    i, img = append_empty_image()
+    frame_info = request_frame()
+
+    if not frame_info:
+        return
+
+    img = frame_info.image
     img_w, img_h = img.size
     draw = ImageDraw.Draw(img)
-    draw.rectangle([(0, 0), (img_w, img_h)], (0, 0, 100))
+    draw.rectangle([(0, 0), (img_w, img_h)], BACKGROUND)
 
     if highlights == None:
         highlights = set()
@@ -111,23 +106,18 @@ def draw_stacks(stacks: list[list[int]], highlights = None):
             y0 = img_h - (BORDER + (BOX_HEIGHT + BORDER) * (row + 2))
             x1 = x0 + BOX_WIDTH
             y1 = y0 + BOX_HEIGHT
-            color = (255, 0, 100) if (col, row) in highlights else (0, 255, 100)
+            color = HIGHLIGHT if (col, row) in highlights else MAIN_FILL
             draw.rectangle([(x0, y0), (x1, y1)], color)
-            draw.text((x0 + ADJUST_X, y0 + ADJUST_Y), letter, (0, 0, 100))
-    draw.text ( (BORDER, img_h - BORDER - BOX_HEIGHT), str(i), font=IMG_FONT, fill=(0, 255, 0, 0) )
-
-def save_gif(filename: str):
-    global images, image_day, unique_counter
-    if images == []:
-        return
+            draw.text(
+                (x0 + ADJUST_X, y0 + ADJUST_Y), 
+                letter, 
+                BACKGROUND)
     
-    path = f"./output/{image_day}/{unique_counter}_{filename}.gif"
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    if os.path.exists(path):
-        print(f'[VIS] Deleting already existing {path}')
-        os.remove(path)
-    images[0].save(path, save_all=True, append_images=images[1:], loop=0, duration=25)
-    print(f"[VIS] Saved {len(images)} images")
+    draw.text(
+        (BORDER, img_h - BORDER - BOX_HEIGHT), 
+        str(frame_info.frame_number), 
+        font=GIF_FONT, 
+        fill=GREEN)
 
 class Instruction:
     move_from: int
---
+++ aoc2022/gif.py
@@ -0,0 +1,91 @@
+from aoc2022.advent import current_day
+from dataclasses import dataclass
+from PIL import Image, ImageFont
+from typing import Optional, Tuple
+
+import os
+import sys
+
+GIF_FONT = ImageFont.truetype("./res/font.ttf")
+
+@dataclass
+class GifBuilder:
+    id: int
+    size: Tuple[int, int]
+    images: list[Image.Image]
+
+@dataclass
+class FrameInfo:
+    frame_number: int
+    image: Image.Image
+
+gif_counter: int = 0
+current_builder: Optional[GifBuilder] = None
+
+def is_drawing() -> bool:
+    return "--paint" in sys.argv
+
+def initialize_gif(width: int, height: int):
+    global current_builder
+
+    current_builder = None
+    if not is_drawing():
+        print("[VIS] Won't paint this time. Set the --images flag to paint the gif.")
+        return
+    current_builder = GifBuilder(
+        id=increment_gif_counter(), 
+        size=(width, height), 
+        images=[])
+
+def request_frame() -> Optional[FrameInfo]:
+    global current_builder
+    
+    if not current_builder:
+        return None
+
+    img = Image.new("RGB", current_builder.size, "black")
+    current_builder.images.append(img)
+    frame_count = len(current_builder.images)
+    if frame_count % 100 == 0:
+        print(f"[VIS] Painted {frame_count} frames already.")
+    return FrameInfo(frame_count, img)
+
+def save_gif(name: Optional[str], subdir: Optional[str] = None, cleanup: bool = True, **kwargs):
+    global current_builder
+    if not current_builder:
+        return
+    
+    if len(current_builder.images) < 1:
+        print("[VIS] Skipping an attempt to save a gif with zero frames.")
+        return
+    
+    if not name:
+        name = "untitled"
+    
+    if not subdir:
+        subdir = f"day{current_day()}"
+    
+    path = f"./output/{subdir}/{current_builder.id}_{name}.gif"
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    if os.path.exists(path):
+        print(f'[VIS] Deleting already existing {path}')
+        os.remove(path)
+
+    images = current_builder.images
+    images[0].save(
+        path, 
+        save_all=True, 
+        append_images=images[1:])
+    print(f"[VIS] Saved {len(images)} images")
+
+    if cleanup:
+        current_builder = None
+
+def current_gif_counter() -> int:
+    global gif_counter
+    return gif_counter
+
+def increment_gif_counter() -> int:
+    global gif_counter
+    gif_counter += 1
+    return gif_counter
