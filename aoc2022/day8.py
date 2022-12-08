c46215146d09add7c8958417e22d9b3518121a04
Ivan Gusiev <igusiev@spotify.com>
2022-12-08 11:45:32 +0100

    template for day 8, started introducing black

---
+++ aoc2022/day8.py
@@ -0,0 +1,26 @@
+from aoc2022.advent import set_day_from_filename
+from aoc2022.util import Input, Grid, clean_lines
+from io import TextIOWrapper
+
+def main():
+    set_day_from_filename(__file__)
+    input = Input.for_advent()
+    for file in [input.test_path, input.challenge_path]:
+        print("input:", file)
+        with open(file, mode='r') as f:
+            solve(f)
+
+def solve(f: TextIOWrapper):
+    lines = clean_lines(f)
+    solve_p1(lines)
+    solve_p2(lines)
+
+def solve_p1(lines: list[str]):
+    grid = Grid.from_lines(lines)
+    print("p1", len(lines))
+
+def solve_p2(lines: list[str]):
+    print("p2", sum(map(len, lines)))
+
+if __name__ == '__main__':
+    main()
--- aoc2022/util.py
+++ aoc2022/util.py
@@ -1,9 +1,12 @@
 from aoc2022.advent import current_day, day_from_filename
+from dataclasses import dataclass
 from io import TextIOWrapper
-from typing import Optional, TypeVar
+from typing import Generic, Optional, Tuple, TypeVar
+
 
 def clean_lines(f: TextIOWrapper) -> list[str]:
     return list(map(str.rstrip, f.readlines()))
+
 
 def split_by_newline(lines: list[str]) -> list[list[str]]:
     result: list[list[str]] = []
@@ -19,12 +22,16 @@ def split_by_newline(lines: list[str]) -> list[list[str]]:
         result.append(current)
     return result
 
-T = TypeVar('T')
+
+T = TypeVar("T")
+
+
 def split_to_chunks(lines: list[T], n) -> list[list[T]]:
     result: list[list[T]] = []
     for i in range(0, len(lines), n):
-        result.append(lines[i:i + n])
+        result.append(lines[i : i + n])
     return result
+
 
 class Input:
     challenge_path: str
@@ -42,12 +49,13 @@ class Input:
         self.challenge_path = f"input/day{day_num}.txt"
 
     @classmethod
-    def for_advent(cls) -> 'Input':
+    def for_advent(cls) -> "Input":
         return cls(current_day())
-    
+
     @classmethod
-    def from_filename(cls, filename: str) -> 'Input':
+    def from_filename(cls, filename: str) -> "Input":
         return cls(day_from_filename(filename))
+
 
 class Output:
     _unique_counter: int = 0
@@ -66,20 +74,77 @@ class Output:
 
         Output._unique_counter += 1
         extension = self.extension or extension or "txt"
-        path = f"./output/{self.subdir}/{Output._unique_counter}_{self.name}.{extension}"
+        path = (
+            f"./output/{self.subdir}/{Output._unique_counter}_{self.name}.{extension}"
+        )
 
         os.makedirs(os.path.dirname(path), exist_ok=True)
         if os.path.exists(path):
-            print(f'[OUT] Deleting already existing {path}')
+            print(f"[OUT] Deleting already existing {path}")
             os.remove(path)
 
         return path
 
     @classmethod
-    def create(cls, subdir: Optional[str] = None, name: Optional[str] = None, extension: Optional[str] = None) -> 'Output':
+    def create(
+        cls,
+        subdir: Optional[str] = None,
+        name: Optional[str] = None,
+        extension: Optional[str] = None,
+    ) -> "Output":
         if not name:
             name = "untitled"
         if not subdir:
             subdir = f"day{current_day()}"
 
         return Output(name, subdir, extension)
+
+
+class Grid(Generic[T]):
+    cells: list[list[T]]
+    width: int
+    height: int
+
+    def __init__(self, width: int, height: int, zero: T):
+        self.width = width
+        self.height = height
+        self.cells = [[zero for _ in range(width)] for _ in range(height)]
+
+    def __getitem__(self, coord: Tuple[int, int]):
+        x, y = coord
+        return self.cells[y][x]
+
+    def __setitem__(self, coord: Tuple[int, int], value: T):
+        x, y = coord
+        self.cells[y][x] = value
+
+    def __repr__(self) -> str:
+        return "\n".join(
+            [
+                "".join([self[x, y] for x in range(self.width)])
+                for y in range(self.height)
+            ]
+        )
+
+    def row(self, y: int) -> list[T]:
+        return self.cells[y]
+
+    def col(self, x: int) -> list[T]:
+        return [self[x, y] for y in range(self.height)]
+
+    def all_cells(self) -> list[T]:
+        return [self[x, y] for x in range(self.width) for y in range(self.height)]
+
+    @classmethod
+    def from_lines(cls, lines: list[str]) -> "Grid[str]":
+        assert len(lines) > 0
+
+        height = len(lines)
+        width = len(lines[0])
+        grid = Grid(width, height, "")
+
+        for y, line in enumerate(lines):
+            for x, char in enumerate(line):
+                grid[x, y] = char
+
+        return grid
